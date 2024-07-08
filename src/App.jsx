66f03171d7f45938f1b19ce384b25ef114add3ef
Akash Kumar <behuriaakash9@gamil.com>
2024-07-08 18:51:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Main from './Components/Main/Main'
+
+const App = () => {
+
+
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
